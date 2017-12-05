ca4da7fb1750f70e52c9b3f1907b6f41d6762fa7
dmgame <dmgame1989@mail.ru>
2017-12-05 12:51:24 +0200

    add socket.io

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Socket
-const client = require('socket.io')(app);
+const client = require('socket.io');
+const io = client(app);
 
 // Connect to mongoose
 mongoose.connect('mongodb://easycode:easycode@ds125146.mlab.com:25146/easycode-testing-db', {
@@ -115,7 +116,7 @@ app.get('/test', (req, res, next) => {
 
 
 // Connect to socket
-client.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('Client connected');
     
     socket.emit('connect');
